829dc313d235021d7c7918a3a1414dab3f4445ab
The Stoned Raider <joebopie@googlemail.com>
2019-11-22 22:20:06 +0000

    Add files via upload

---
+++ SRcctc/config.lua
@@ -0,0 +1,14 @@
+Config                            = {}
+
+Config.TCon = true  --- is TC avaliable 
+
+Config.CCon = true   ------ is XX avaliable 
+
+Config.TCkey = 20    ---- Z
+
+Config.CCkey = 249    ---- N
+
+
+Config.Maxspeed = 34.0   ----In meters per second 34 = 75mph
+
+Config.Minspeed = 0.1
