84b01305ed61fe19ac48837fef3c9261fe97eafd
The Stoned Raider <joebopie@googlemail.com>
2019-11-24 00:51:22 +0000

    more options for UI and TC

--- SRcctc/config.lua
+++ SRcctc/config.lua
@@ -12,6 +12,23 @@ Config.Maxspeed = 34.0 --- In meters per second 34 = 75mph
 
 Config.Minspeed = 0.1 --- In meters per second 0.1 = 0.2mph
 
+Config.action = 0.2     --- How much the TC wil try to stop you sliding Lower = more help
+
 Config.OnScreenText = true --- Enable/Disable On-Screen text
 
-Config.Watermark = false --- Enable/Disable Watermark
+Config.OnScreendisplayTC = true --- Enable/Disable On-Screen display/colours
+
+Config.Watermark = true --- Enable/Disable Watermark
+
+ UITC = {
+
+	x =  0.82 ,	-- TC Screen Coords 	0.0-1.0 left to right 
+	y = 0.720 ,	-- TC Screen Coords 	0.0-1.0 = top to bottom
+
+}
+ UI = {
+
+	x =  0.570 ,	-- CC Screen Coords 	0.0-1.0 left to right 
+	y = 0.440 ,	-- CC Screen Coords 	0.0-1.0 = top to bottom
+
+}
